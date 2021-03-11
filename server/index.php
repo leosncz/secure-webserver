db5971315b23f014d6eed00d41ecefae678ea840
Sanchez Léo <leosa@hotmail.fr>
2021-03-11 17:21:20 +0100

    Add files via upload

---
+++ server/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "Hola from PHP :) hehe \n";
+?>
--- server/server.h
+++ server/server.h
@@ -12,20 +12,45 @@ public:
 
  bool start(){
   svr.Get(R"(/(.*))", [&](const Request& req, Response& res) {
-    auto fileToLook = req.matches[1];
-    std::ifstream t(fileToLook);
-    std::string fileContent((std::istreambuf_iterator<char>(t)),std::istreambuf_iterator<char>());
-    //encode the result
-    std::vector<unsigned char> key = {chosenKey[0],chosenKey[1],chosenKey[2],chosenKey[3],chosenKey[4],chosenKey[5],chosenKey[6],chosenKey[7],chosenKey[8],chosenKey[9],chosenKey[10],chosenKey[11],chosenKey[12],chosenKey[13],chosenKey[14],chosenKey[15]};
-    const unsigned char iv[16] = {
-        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-        0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
-    };
-    const unsigned long encrypted_size = plusaes::get_padded_encrypted_size(fileContent.size());
-    std::vector<unsigned char> encrypted(encrypted_size);
-    plusaes::encrypt_cbc((unsigned char*)fileContent.data(), fileContent.size(), &key[0], key.size(), &iv, &encrypted[0], encrypted.size(), true);
-    fileContent = string(encrypted.begin(), encrypted.end());
-    res.set_content(fileContent, "text/html");    
+    string fileToLook = req.matches[1];
+    if(fileToLook.size() > 3){
+      if(fileToLook[fileToLook.size() - 3] == 'p' && fileToLook[fileToLook.size() - 2] == 'h' && fileToLook[fileToLook.size() - 1] == 'p') // php file
+      {
+        string commandLine = "php "; 
+        commandLine.append(fileToLook);
+        commandLine.append(" > phpResult");
+        system(commandLine.c_str());
+        std::ifstream t("phpResult");
+        std::string fileContent((std::istreambuf_iterator<char>(t)),std::istreambuf_iterator<char>());
+        //encode the result
+        std::vector<unsigned char> key = {chosenKey[0],chosenKey[1],chosenKey[2],chosenKey[3],chosenKey[4],chosenKey[5],chosenKey[6],chosenKey[7],chosenKey[8],chosenKey[9],chosenKey[10],chosenKey[11],chosenKey[12],chosenKey[13],chosenKey[14],chosenKey[15]};
+        const unsigned char iv[16] = {
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+        };
+        const unsigned long encrypted_size = plusaes::get_padded_encrypted_size(fileContent.size());
+        std::vector<unsigned char> encrypted(encrypted_size);
+        plusaes::encrypt_cbc((unsigned char*)fileContent.data(), fileContent.size(), &key[0], key.size(), &iv, &encrypted[0], encrypted.size(), true);
+        fileContent = string(encrypted.begin(), encrypted.end());
+        res.set_content(fileContent, "text/html");
+      }
+      else // Not a php file
+      {
+        std::ifstream t(fileToLook);
+        std::string fileContent((std::istreambuf_iterator<char>(t)),std::istreambuf_iterator<char>());
+        //encode the result
+        std::vector<unsigned char> key = {chosenKey[0],chosenKey[1],chosenKey[2],chosenKey[3],chosenKey[4],chosenKey[5],chosenKey[6],chosenKey[7],chosenKey[8],chosenKey[9],chosenKey[10],chosenKey[11],chosenKey[12],chosenKey[13],chosenKey[14],chosenKey[15]};
+        const unsigned char iv[16] = {
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+        };
+        const unsigned long encrypted_size = plusaes::get_padded_encrypted_size(fileContent.size());
+        std::vector<unsigned char> encrypted(encrypted_size);
+        plusaes::encrypt_cbc((unsigned char*)fileContent.data(), fileContent.size(), &key[0], key.size(), &iv, &encrypted[0], encrypted.size(), true);
+        fileContent = string(encrypted.begin(), encrypted.end());
+        res.set_content(fileContent, "text/html");
+      }
+    } 
   });
   port = svr.bind_to_any_port("0.0.0.0");
   cout << "-> Listening started on port " << port << " !" << endl;
